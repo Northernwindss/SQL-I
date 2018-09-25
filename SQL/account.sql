a4beedc5a1bdc4836b193969fd20943c36f8b093
rasbandit <todd.rasband@gmail.com>
2018-09-25 12:32:02 -0600

    first

---
+++ SQL/account.sql
@@ -0,0 +1,31 @@
+CREATE TABLE account (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(25) NOT NULL,
+	email text
+);
+
+insert into account (name, email) values ('Kenn', 'klambshine0@quantcast.com');
+insert into account (name, email) values ('Jehanna', 'jilliston1@so-net.ne.jp');
+insert into account (name, email) values ('Anna', 'abotting2@ask.com');
+insert into account (name, email) values ('Oliver', 'opoxson3@de.vu');
+insert into account (name, email) values ('Aimil', 'amulryan4@jugem.jp');
+insert into account (name, email) values ('Karry', 'kcrecy5@chronoengine.com');
+insert into account (name, email) values ('Eustacia', 'ewardale6@about.com');
+insert into account (name, email) values ('Thaxter', 'tadamczyk7@163.com');
+insert into account (name, email) values ('Adaline', 'astleger8@ameblo.jp');
+insert into account (name, email) values ('Libbie', 'lhinkensen9@booking.com');
+insert into account (name, email) values ('Fayth', 'fthoumassona@soundcloud.com');
+insert into account (name, email) values ('Dana', 'dblundanb@twitter.com');
+insert into account (name, email) values ('Lorry', 'lbuddenc@yellowbook.com');
+insert into account (name, email) values ('Bronnie', 'bjewsterd@360.cn');
+insert into account (name, email) values ('Vivie', 'vhamele@answers.com');
+insert into account (name, email) values ('Kiley', 'kmacgettigenf@google.com');
+insert into account (name, email) values ('Kylynn', 'kbarmeg@hp.com');
+insert into account (name, email) values ('Myrwyn', 'mtedahlh@barnesandnoble.com');
+insert into account (name, email) values ('Spense', 'sdouchei@yandex.ru');
+insert into account (name, email) values ('Winifred', 'wculyj@linkedin.com');
+insert into account (name, email) values ('Theobald', 'tgoodlipk@cbc.ca');
+insert into account (name, email) values ('Georgi', 'gmowsdelll@google.com.au');
+insert into account (name, email) values ('Algernon', 'ademkem@arstechnica.com');
+insert into account (name, email) values ('Doro', 'dstegelln@techcrunch.com');
+insert into account (name, email) values ('Caitlin', 'cmiello@ning.com');
